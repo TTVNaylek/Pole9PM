33455461d14a6231ee368a9ee85dda863bc33464
Kelyan Desmet <kelyan.desmet1@gmail.com>
2024-02-02 08:34:45 +0100

    Nettoyage du code

--- dist/ServerModule.js
+++ dist/ServerModule.js
@@ -7,9 +7,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.prisma = void 0;
 //Import des dépendaces requises
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
 const http_1 = __importDefault(require("http"));
+//Création d'un objet de prisma (ORM pour la BDD)
+exports.prisma = new client_1.PrismaClient();
 //Déclarations des constantes nécessaires au fonctionnement du serveur web
 const PORT = 80;
 const HOST = "172.17.50.129";
@@ -17,6 +21,7 @@ const app = (0, express_1.default)();
 //Lors de la réception de la requête, cela affiche "Server ON" sur la page web
 app.get('/', (req, res) => {
     res.send('Server ON');
+    console.log('Server ON');
 });
 //Traitement en cas d'erreur sur le serveur
 app.use((err, req, res, next) => {
@@ -28,7 +33,7 @@ const httpServer = http_1.default.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Functionnal test");
 });
-//Écoute sur le port 80 le serveur web
+//Écoute sur l'adresse IP ... et le port 80 le serveur web
 app.listen(PORT, HOST, () => {
     console.log(`App is listenting on port ${HOST}:${PORT}`);
 });
--- src/ServerModule.ts
+++ src/ServerModule.ts
@@ -5,10 +5,9 @@
 
 //Import des dépendaces requises
 import express, {Application, NextFunction, Request, Response} from "express";
-//Test DB & Prisma
 import {PrismaClient} from "@prisma/client";
 import http from "http";
-//Test DB & Prisma
+//Création d'un objet de prisma (ORM pour la BDD)
 export const prisma = new PrismaClient();
 
 //Déclarations des constantes nécessaires au fonctionnement du serveur web
@@ -16,16 +15,10 @@ const PORT = 80;
 const HOST = "172.17.50.129";
 const app = express();
 
-//Test DB & Prisma
-async function main() {
-
-  // ... you will write your Prisma Client queries here
-
-}
-
 //Lors de la réception de la requête, cela affiche "Server ON" sur la page web
 app.get('/', (req: Request, res: Response) => {
     res.send('Server ON');
+    console.log('Server ON');
 });
 
 //Traitement en cas d'erreur sur le serveur
@@ -34,23 +27,13 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send("Error on express server");
 });
 
-
 //Service HTTP
 const httpServer = http.createServer((req, res)=>{
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Functionnal test");
 });
   
-//Écoute sur le port 80 le serveur web
+//Écoute sur l'adresse IP ... et le port 80 le serveur web
 app.listen(PORT, HOST, () => {
     console.log(`App is listenting on port ${HOST}:${PORT}`)
 });
-
-//Test DB & Prisma
-main().then(async () => {
-  await prisma.$disconnect()
-}).catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
